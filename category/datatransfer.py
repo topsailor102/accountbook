ebcc80715bc27afd0c3107b358e1b352277666f9
Won Young Chi <wonyoung.chi@gmail.com>
2020-05-15 20:12:24 +0200

    1. hide credentials 2. change database (sqlite3 to postgresql in AWS) 3. modify the function related to import csv file

--- category/datatransfer.py
+++ category/datatransfer.py
@@ -1,6 +1,7 @@
 import sqlite3
+import psycopg2
 import csv
-import os
+import os, json
 
 db_name = "db.sqlite3"
 
@@ -23,17 +24,21 @@ def get_date_from_the_file(csvfile):
                 creationinfo = row['CREATIONINFO']
                 sector_id = row['SECTOR']
                 way_id = row['WAY']
+                insert_query = f'INSERT INTO category_expense (DATEINFO, PLACE, COST, SUMMARY, ISFIXED, CREATIONINFO, SECTOR_ID, WAY_ID) \
+                                 VALUES (\'{dateinfo}\', \'{place}\', {cost}, \'{summary}\', {isfixed}, \'{creationinfo}\', {sector_id}, {way_id});\n'
+                """
                 insert_query = f'INSERT INTO category_expense (\'DATEINFO\', \'PLACE\', \'COST\', \'SUMMARY\',\
                                  \'ISFIXED\', \'CREATIONINFO\', \'SECTOR_ID\', \'WAY_ID\') \
                                  VALUES (\'{dateinfo}\', \'{place}\', {cost}, \'{summary}\', \
                                          {isfixed}, \'{creationinfo}\', {sector_id}, {way_id});\n'
-
+                """
                 query_set += insert_query
         #print(query_set)
         insert_data_to_db(query_set, db_name)
+
         return query_set
 
-def insert_data_to_db(query, db_name):
+def insert_data_to_sqlite3(query, db_name):
     con = sqlite3.connect(db_name)
     c = con.cursor()
     
@@ -44,3 +49,31 @@ def insert_data_to_db(query, db_name):
         
     c.close()
     con.close()    
+
+def insert_data_to_db(query, db_name):
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    secret_file = os.path.join(BASE_DIR, 'secrets.json')
+    with open(secret_file) as f:
+        secrets = json.loads(f.read())
+    
+    #Establishing the connection
+    conn = psycopg2.connect(
+        database=secrets['NAME'], user=secrets['USER'], password=secrets['PASSWORD'], host=secrets['HOST'], port= secrets['PORT']
+    )
+    #Setting auto commit false
+    conn.autocommit = True
+
+    #Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+
+    # Preparing SQL queries to INSERT a record into the database.
+    cursor.execute(query)
+    #print(query)
+
+    # Commit your changes in the database
+    conn.commit()
+    print("Records inserted........")
+
+    # Closing the connection
+    conn.close()
